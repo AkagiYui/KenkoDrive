8e963ae8113ee9030e9bd0758d0aa6059323ed0c
AkagiYui <akagiyui@yeah.net>
2023-05-31 16:58:58 +0800

    feat: create project

---
+++ src/main/java/com/akagiyui/drive/KenkoDriveApplication.java
@@ -0,0 +1,13 @@
+package com.akagiyui.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenkoDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenkoDriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akagiyui/drive/KenkoDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akagiyui.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenkoDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
