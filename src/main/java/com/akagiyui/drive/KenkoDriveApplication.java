a35094a70d9b7886c60b0c9588f6d4d9b476873a
AkagiYui <akagiyui@yeah.net>
2023-06-26 21:18:26 +0800

    chore: add comment

--- src/main/java/com/akagiyui/drive/KenkoDriveApplication.java
+++ src/main/java/com/akagiyui/drive/KenkoDriveApplication.java
@@ -3,6 +3,10 @@ package com.akagiyui.drive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Kenko Drive Application
+ * @author AkagiYui
+ */
 @SpringBootApplication
 public class KenkoDriveApplication {
 
