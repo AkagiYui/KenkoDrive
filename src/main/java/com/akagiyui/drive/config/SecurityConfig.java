5bf5203b0bb94baec4ab0cfcf683a9dd8abe22e0
AkagiYui <akagiyui@yeah.net>
2023-06-26 21:17:02 +0800

    feat: add spring security and cors filter

---
+++ src/main/java/com/akagiyui/drive/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.akagiyui.drive.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Spring Security 配置类
+ * @author AkagiYui
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * 配置 Spring Security
+     * @param http HttpSecurity
+     * @return SecurityFilterChain
+     * @throws Exception 异常
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(a -> a
+                        // 允许所有请求
+                        .anyRequest().permitAll()
+                )
+                // 关闭 CSRF
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    /**
+     * 配置跨域
+     * @return CorsFilter
+     */
+    @Bean
+    public CorsFilter corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsFilter(source);
+    }
+}
