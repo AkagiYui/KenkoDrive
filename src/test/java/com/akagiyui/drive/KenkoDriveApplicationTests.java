033df6f36b0908321f6d37af48d35b34950db364
AkagiYui <akagiyui@yeah.net>
2023-07-24 18:43:30 +0800

    feat: add test config for gradle test

--- src/test/java/com/akagiyui/drive/KenkoDriveApplicationTests.java
+++ src/test/java/com/akagiyui/drive/KenkoDriveApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 class KenkoDriveApplicationTests {
 
     @Test
