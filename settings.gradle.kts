9528c76e35e36d824a66855208fe15f27a1e1762
AkagiYui <akagiyui@yeah.net>
2024-04-03 23:38:43 +0800

    build: add gradle plugin repository mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin/")
+        }
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     // https://github.com/gradle/foojay-toolchains
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0" // 工具链下载仓库
