0b995160d127f37478990c480ca63bd92a3d012e
AkagiYui <akagiyui@yeah.net>
2024-04-16 00:19:11 +0800

    test: add first kotlin test case

---
+++ src/test/kotlin/com/akagiyui/drive/controller/SystemControllerTests.kt
@@ -0,0 +1,37 @@
+package com.akagiyui.drive.controller
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+/**
+ * SystemController 测试类
+ *
+ * @author AkagiYui
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SystemControllerTests(@Autowired val restTemplate: TestRestTemplate) {
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
+    @Test
+    fun `Assert system version, content and status code`() {
+        println(">> Assert system version, content and status code")
+        val entity = restTemplate.getForEntity<String>("/system/version")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("\"code\":10000")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
+}
