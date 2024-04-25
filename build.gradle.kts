2910f0a6fe99dcd0373e24385b4f98a66e17699b
AkagiYui <akagiyui@yeah.net>
2024-04-26 00:32:15 +0800

    chore: rename Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,6 @@ version = "0.0.1" // 项目版本
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-        vendor = JvmVendorSpec.GRAAL_VM
-    }
 }
 
 allOpen {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,12 +8,3 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
-plugins {
-    /**
-     * https://github.com/gradle/foojay-toolchains
-     * 通过 Foojay Toolchains 插件，Gradle 可以自动下载并使用 Foojay 提供的 JDK
-     * 无需手动下载 JDK，也无需配置环境变量
-     */
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
